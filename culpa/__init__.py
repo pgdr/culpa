a52ec73c303348c1163af4a2ff6d9387f8537835
Pål Grønås Drange <PGDR@equinor.com>
2019-04-09 12:57:42 +0200

    Add nicer output for columns

--- culpa/__init__.py
+++ culpa/__init__.py
@@ -3,10 +3,10 @@ import xlrd
 
 def main(fname):
     df = pd.read_excel(fname)
-    return df.columns
+    return [str(x) for x in list(df.columns)]
 
 if __name__ == '__main__':
     from sys import argv
     if len(argv) < 2:
         exit('Usage: app arg')
-    print(main(argv[1]))
+    print('\n'.join(main(argv[1])))
