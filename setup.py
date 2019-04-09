3ccb3db7313e5a06becd0b5d1f4f96ad1b0c5949
Pål Grønås Drange <PGDR@equinor.com>
2019-04-09 22:37:53 +0200

    Add tests, travis and LICENSE

--- setup.py
+++ setup.py
@@ -1,11 +1,18 @@
 from setuptools import setup
 
+def _reqs():
+    reqs = []
+    with open('requirements.txt', 'r') as f:
+        reqs = [pkg.strip() for pkg in f.readlines()]
+    return reqs
+
 setup(
     name='culpa',
     packages=['culpa'],
-    version='0.0.2',
-    install_requires=['pandas', 'xlrd'],
+    version='0.0.3',
+    install_requires=_reqs(),
     entry_points={
         'console_scripts': ['culpa=culpa:main']
-        }
+        },
+    test_suite='tests',
     )
---
+++ tests/test_culpa.py
@@ -0,0 +1,23 @@
+import os
+import unittest
+import culpa
+
+def here(path):
+    here_ = os.path.dirname(os.path.abspath(__file__))
+    return os.path.join(here_, path)
+
+
+class TestCulpa(unittest.TestCase):
+
+    def test_columns(self):
+        act = culpa.read_file(here('test.xlsx'))
+        exp = ['alpha', 'beta', 'charlie', 'delta', 'echo']
+        self.assertEqual(exp, act)
+
+    def test_col_content(self):
+        act = culpa.read_col(here('test.xlsx'), 'charlie')
+        exp = ['alpha', 'beta', 'delta', 'epsilon', 'eta', 'gamma', 'zeta']
+        self.assertEqual(exp, act)
+
+if __name__ == '__main__':
+    unittest.main()
